1acb0e67c5e81bdb860c21d60078e9885bac72c3
cognaclee <44490968+cognaclee@users.noreply.github.com>
2025-08-17 13:24:48 +0800

    Update obj2img.py

--- preprocess/obj2img.py
+++ preprocess/obj2img.py
@@ -12,8 +12,6 @@ from shap_e.models.download import load_model
 from shap_e.util.data_util import load_or_create_multimodal_batch
 from shap_e.util.notebooks import create_pan_cameras, decode_latent_images, gif_widget
 
-import os
-import time
 
 CUR_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -30,44 +28,13 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]=str(FLAGS.GPU)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#Display a batch of images inline.
+
+
 def save_images(batch,save_dir,base_name):
-    
     for i in range(len(batch)):
         image_pil=batch[i]
         img_name = os.path.join(save_dir, base_name+str(i)+'.png')
         image_pil.save(img_name)
-
-
-def load_shapenet(lst_dir, cats, mesh_dir):
-
-    # sanity check: all files exists
-    for catnm in cats.keys():
-
-        print(f'[*] checking obj files in {catnm} ({cats[catnm]})')
-
-        cat_id = cats[catnm]
-        cat_mesh_dir = os.path.join(mesh_dir, cat_id)
-        with open(lst_dir+"/"+str(cat_id)+"_test.lst", "r") as f:
-            list_obj = f.readlines()
-
-        with open(lst_dir+"/"+str(cat_id)+"_train.lst", "r") as f:
-            list_obj += f.readlines()
-
-        list_obj = [f.rstrip() for f in list_obj]
-        list_obj = [f'{cat_mesh_dir}/{f}/model.obj' for f in list_obj]
-
-        for f in list_obj:
-            if not os.path.exists(f):
-                print(f)
-                import pdb; pdb.set_trace()
-            assert os.path.exists(f)
-
-
-        print(f'[*] all files exist for {catnm} ({cats[catnm]})!')
-    return list_obj
-
-
 
 
 if __name__ == "__main__":
@@ -99,7 +66,6 @@ if __name__ == "__main__":
         #basename = text_cont[k][:240] + '_text_120.obj'
         
         mesh_file = os.path.join(FLAGS.data_dir,basename)
-        
         if not os.path.exists(mesh_file):
             print('no',mesh_file)
             continue
@@ -122,11 +88,7 @@ if __name__ == "__main__":
         os.remove(file_name)
 
         with torch.no_grad():
-            start_time = time.time()
             latent = xm.encoder.encode_to_bottleneck(batch)
             images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
             baseName = bf.basename(mesh_file)[:240]
             save_images(images,save_dir,baseName)
-            end_time = time.time()
-            run_time=end_time - start_time
-            #print(f"!!! Run time is: {run_time} s")
